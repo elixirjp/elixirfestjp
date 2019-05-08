6994c683d5d778584ad640476b9200bebe71337f
hfunai <hfunai@tsukinowa.jp>
2019-05-09 01:09:18 +0900

    ショートセッションのフラグを追加する

--- apps/core/models.py
+++ apps/core/models.py
@@ -29,15 +29,16 @@ class News(models.Model):
 
 
 class Session(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    speaker = models.CharField(max_length=100)
-    speaker_icon = models.ImageField(upload_to='speaker')
-    speaker_icon2 = models.ImageField(upload_to='speaker', blank=True)
-    twitter_link = models.URLField(blank=True)
-    order = models.IntegerField(default=0)
-    is_keynote = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    title = models.CharField('タイトル', max_length=255)
+    content = models.TextField('セッション内容')
+    speaker = models.CharField('スピーカー', max_length=100)
+    speaker_icon = models.ImageField('スピーカーのアイコン', upload_to='speaker')
+    speaker_icon2 = models.ImageField('スピーカー2のアイコン', upload_to='speaker', blank=True)
+    twitter_link = models.URLField('TwitterのURL', blank=True)
+    order = models.IntegerField('並び順', default=0)
+    is_keynote = models.BooleanField('KEYNOTE セッション', default=False)
+    is_short_session = models.BooleanField('ショートセッション', default=False)
+    is_active = models.BooleanField('サイトに表示', default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
